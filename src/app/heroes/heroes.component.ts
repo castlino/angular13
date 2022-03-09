44025a693e8597ee4f9c2f634daa830765c9c762
LiNo <felino@mybos.com>
2022-03-10 00:09:25 +1100

    5. Add Navigation (https://angular.io/tutorial/toh-pt5)   - Added Routing ($ ng generate module app-routing --flat --module=app)   - Added navigation, @ActivatedRoute()   -@routerLink (https://angular.io/tutorial/toh-pt5#add-a-navigation-link-routerlink)   - Added dashboard component to hold navigation template with links   - Added default navigation route, (  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },)   - Added 'Back' button (@Location)

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,19 +10,12 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
-  selectedHero?: Hero;
 
   constructor(private heroService: HeroService,
               public messageService: MessageService) {}
 
-
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add('HeroesComponent: selected a hero('+ hero.name +')...');
   }
 
   getHeroes(): void {
